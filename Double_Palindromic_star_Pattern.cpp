48a10e628c6fcbd3c00f5c7aca878cadfbccc598
Yash Kumar Sharma <Yashsharm884@gmail.com>
2021-06-13 14:27:59 +0530

    Star Pattern
        *
       ***
      *****
     *******
    *********
    *********
     *******
      *****
       ***
        *

---
+++ Double_Palindromic_star_Pattern.cpp
@@ -0,0 +1,64 @@
+/*
+
+    *
+   ***
+  *****
+ *******
+*********
+*********
+ *******
+  *****
+   ***
+    *
+
+*/ 
+
+
+#include<bits/stdc++.h>
+using namespace std; 
+
+int main()
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); 
+
+    int row;
+    cin>>row; 
+
+    for(int i = 1; i <= row; i++)
+    {
+        for(int j = 1; j <= row - i ; j++) 
+        {
+            cout<<" "; 
+        }
+        for(int k = 1; k <= 2 * i - 1; k++ ) 
+        {
+            cout<<"*"; 
+        }
+        cout<<endl; 
+    }
+
+    for(int i = row; i >= 1; i-- ) 
+    {
+        for(int j = 1; j <= row - i ; j++) 
+        {
+            cout<<" "; 
+        }
+        for(int k = 1; k <= 2 * i - 1; k++ ) 
+        {
+            cout<<"*"; 
+        }
+        cout<<endl; 
+    }
+
+
+
+
+
+
+
+
+
+
+    return 0; 
+}
