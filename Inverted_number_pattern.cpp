61daf775d9854666cf722fe1cb7e0345b40a8464
Yash Kumar Sharma <Yashsharm884@gmail.com>
2021-06-13 13:05:45 +0530

    Inverted Number Pattern
    1 2 3 4 5
    1 2 3 4
    1 2 3
    1 2
    1

---
+++ Inverted_number_pattern.cpp
@@ -0,0 +1,37 @@
+/*
+
+1 2 3 4 5 
+1 2 3 4
+1 2 3
+1 2
+1
+
+*/
+
+
+
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); 
+
+    int row; 
+    cin>>row; 
+
+
+    for(int i = 1; i <= row; i++)
+    {
+        for(int j = 1; j <= row + 1 - i; j++)  
+        {
+            cout<<j<<" "; 
+        }
+        cout<<endl;
+    }
+
+    return 0; 
+}
+
