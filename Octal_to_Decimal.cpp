4c6a4ba08439a216e9d37d7242e21024136a0398
Yash Kumar Sharma <Yashsharm884@gmail.com>
2021-06-15 22:53:46 +0530

    Octal to Decimal using Functions

---
+++ Octal_to_Decimal.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+int Octal_to_Decimal(int n)
+{
+    int ans; 
+    int x = 1; 
+    while (n>0)
+    {
+        int y = n % 10; 
+        ans += x*y; 
+        x *= 8; 
+        n = n / 10; 
+        
+    }
+    
+    return ans; 
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); 
+    int n; 
+    cin >> n;
+    cout << Octal_to_Decimal(n) << endl; 
+    return 0; 
+}
